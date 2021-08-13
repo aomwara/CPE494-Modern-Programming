2af7eeab6f70d819ba096b8a16a07d43127dcdf5
waranat suttikarn <waranat.cpe@gmail.com>
2021-08-13 22:34:14 +0700

    feat: lab1 structure

---
+++ lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle(5, 7);
+        System.out.println(s1.getArea());
+        System.out.println(s1.getPerimeter());
+    }
+}
---
+++ lab1/Shape.java
@@ -0,0 +1,9 @@
+package lab1;
+
+interface Shape {
+
+    double getArea();
+
+    double getPerimeter();
+
+}
