8f8a1e6d28e1c04a997857f1f56c7a9e1bc48087
waranat suttikarn <waranat.cpe@gmail.com>
2021-08-13 22:43:22 +0700

    feat: add circle

---
+++ lab1/Circle.java
@@ -0,0 +1,19 @@
+package lab1;
+
+public class Circle implements Shape {
+    private double width;
+    private double height;
+
+    public Circle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+}
